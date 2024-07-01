320b7cab5ea95249c9ae9572002a46cfc85bac2f
Kafeev95 <andre.ms@web.de>
2024-07-01 10:43:05 +0000

    02/01 Wertzuweisung II + Datentypen  ++

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -15,9 +15,30 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung | assignment
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName +" "+ familyName + "!"); // soll --> Hi, ich bin Max Mütze!
+//let firstName; // Deklaration (Definition)
+//console.log(firstName); // Ausgabe
+//firstName = "Max"; // Wertzuweisung | assignment
+//console.log(firstName); // Ausgabe
+//let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName +" "+ familyName + "!"); // soll --> Hi, ich bin Max Mütze!
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+let test; //Deklaration
+
+/* undefined */
+
+
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
+//test = "hi";
+//test = 2;
+
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
