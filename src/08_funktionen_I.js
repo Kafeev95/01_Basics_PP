e85e34dace55a14b6960b8c4f5048fbfa5934cd5
Kafeev95 <andre.ms@web.de>
2024-07-04 08:20:48 +0000

    08/01 ++

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,3 +2,14 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsrupf / call
+test();
+
+// Funktionsrupf / callee
+// Funktionsdeklaration
+
+function test() 
+{
+    console.log("Haollo, Marius!");
+}
+
