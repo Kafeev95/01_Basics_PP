546c84f897fdb703a6a4615d986b55f79e16ca82
Kafeev95 <andre.ms@web.de>
2024-07-05 09:00:41 +0000

    09/03 3. variante für divide ++

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -37,24 +37,24 @@ function multiply(a,b)
 {return  a * b;}
 
 // module: division a / b |  test:
-// output(divide(4,2));
-// output(divide(3,2));
-// output(divide(3,-2));
-// output(divide(0,2));
-// output(divide(3,0));
-// output(divide(0,0));
+output(divide(4,2));
+output(divide(3,2));
+output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
 
 function divide(a,b) 
+
+
+
 {
-	if (b!=0) {
-		return  a / b;
-	} 
-		else { return ERROR_STR_DIV;
-	}
-	
-	}
+	if (b==0) {
+			return ERROR_STR_DIV; // Ausnahme
+		}
 
-
+	return  a / b; // Regel
+}
 
 // module: output | test:
 // output("hello");
