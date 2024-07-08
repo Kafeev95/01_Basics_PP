b62f648d6c35478719381150d5faf4676d6bbd92
Kafeev95 <andre.ms@web.de>
2024-07-08 09:08:26 +0000

    11/05 Theorie: Schleifen

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -20,6 +20,28 @@ let arr;
 // output(arr[0]); // Index 0 (1. pos. im Arry)
 // output(arr[arr.length-1]); // letze Pos. / allgemein
 
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 5; i++) { // i --> Schleifen-Index
+//     output(i)
+    
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+
+// for (let i = 10; i > 0; i--) { // i --> Schleifen-Index
+//     output(i);
+
+// }
+
+// Inkrement (var. Schrittweite)
+for (let i = 10; i <= 100; i += 5) 
+{
+   output(i);   
+}
+
 
 
 /**
@@ -47,12 +69,13 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 }
 
 
+
 /*** 01a. Funktionalität mit Array 1 */
 // Kritik | Überlegung
 // word1,word2, .... wordN :: Semantische Struktur
 // arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
 
 function getSentenceArr(arr) {
     const GAP = " ";
