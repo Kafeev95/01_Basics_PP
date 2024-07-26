3168489b231caab815366c1b838eda5c1b4a7b11
Kafeev95 <andre.ms@web.de>
2024-07-26 07:39:39 +0000

    Test

---
+++ src/Test.js
@@ -0,0 +1,52 @@
+// let arr = [true, false, true, false];
+
+// console.log(arr[0]);
+
+// let obj = {
+//     level1: {
+//         param1: 12,
+//         level2: {
+//             param2: [24, 26]
+//         }
+//     }
+// };
+
+// // Ausgeben des Inhalts der property param1
+// console.log(obj.level1.param1);
+
+// // Ausgeben des letzten Elements der property param2
+// console.log(obj.level1.level2.param2[obj.level1.level2.param2.length - 1]);
+
+// Funktion zur Berechnung der Summe
+// function summe(a, b) {
+//     return a + b;
+// }
+
+// // Funktion zur Berechnung des Produkts
+// function produkt(a, b) {
+//     return a * b;
+// }
+
+// // Hauptfunktion rechner
+// function rechner(num1, num2, op) {
+//     let ergebnis;
+    
+//     // Überprüfen, welcher Operator angegeben wurde und entsprechende Funktion aufrufen
+//     if (op == "summe") {
+//         ergebnis = summe(num1, num2);
+//     } else if (op == "produkt") {
+//         ergebnis = produkt(num1, num2);
+//     } else {
+//         console.log("Ungültige Operation");
+//         return;
+//     }
+
+//     // Ausgabe des Ergebnisses in der Konsole
+//     console.log("Das Ergebnis lautet: " + ergebnis);
+// }
+
+// // Funktionscalls
+// rechner(5, 5, "summe");    // Ausgabe: Das Ergebnis lautet: 8
+// rechner(5, 5, "produkt");  // Ausgabe: Das Ergebnis lautet: 15
+
+
